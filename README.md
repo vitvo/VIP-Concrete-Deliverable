c3d75a6d4b137b4c8f82d7f1f87219f5c830e144
schittarath3 <souliya3chittarath@gmail.com>
2020-04-07 16:00:12 -0400

    initial

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1> VIP Concrete Image Processing </h1>
+
+Overview: Code to process CT scans of concrete into .stl files.
+
+<h2> Repo Contents: </h2>
+
+1. Example Folder: Contains input images and outputs
+	1. Original Images- Contain original images given from VIP Mentor
+	2. Split Images- output from image_splitter.py
+	3. Binary Images - output from split_to_binary.m
+	4. bl_54_94.stl - output from Fiji (ImageJ) .stl creator; encoded in binary
+	5. bl_54_94_MM.stl - output from Meshmixer after removing artifacts and reducing
+	
+2. image_splitter.py - Python code used to split each original image into the four quadrants
+	1. Requirements:
+		1. Python 3.7.6
+		2. opencv2
+		3. os
+		4. numpy
+		
+3. split_to_binary - MATLAB code to convert split images into binary images of aggregates
+	1. Requirements
+		1. MATLAB (version used was 2019b)
+		2. Image Batch Processor App (part of Image Processing and Computer Vision package)
+
+<h2> Other Programs: </h2>
+1. Fiji- ImageJ distribution with many plugins for scientific analysis
+2. (Optional) Meshmixer
+
+Note: Detailed description of the functions of each code is described in comments
+	
---
+++ image_splitter.py
@@ -0,0 +1,70 @@
+#load dependencies
+import cv2
+import os
+import numpy as np
+
+
+def get_contours(species_image):
+        image = cv2.imread(species_image)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        inverted = (255-gray)
+
+        #create threshold; original min value = 215
+        thresh = cv2.threshold(inverted, 195,255, cv2.THRESH_TOZERO_INV)[1]
+
+        # Find contours
+        cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+        return cnts
+
+def image_split(image, mainPath, image_name, cnts):
+    image = cv2.imread(image)
+    original = image.copy()
+    
+    #create directories
+    outpath = os.path.join(mainPath, "Final Images")
+    outpath_bl = os.path.join(outpath, "bottom_left")
+    outpath_br = os.path.join(outpath, "bottom_right")
+    outpath_tl = os.path.join(outpath, "top_left")
+    outpath_tr = os.path.join(outpath, "top_right")
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
+        os.makedirs(outpath_bl)
+        os.makedirs(outpath_br)
+        os.makedirs(outpath_tl)
+        os.makedirs(outpath_tr)
+
+    #Iterate thorugh contours and returns split images
+    thold_area = 5000
+    image_number = 1
+    for c in cnts:
+        area = cv2.contourArea(c)         
+        if area > thold_area:  
+            rect = cv2.minAreaRect(c)
+            box = np.int0(cv2.boxPoints(rect))
+            width = int(rect[1][0])
+            height = int(rect[1][1])
+            src_pts = box.astype("float32")
+            dst_pts = np.array([[0, height-1],
+                            [0, 0],
+                            [width-1, 0],
+                            [width-1, height-1]], dtype="float32")
+            matrix = cv2.getPerspectiveTransform(src_pts, dst_pts)
+            warped = cv2.warpPerspective(original, matrix, (width, height))
+            if image_number  == 1:
+                cv2.imwrite((os.path.join(outpath_bl, "bl_{}".format(image_name))), warped)
+            elif image_number == 2:
+                cv2.imwrite((os.path.join(outpath_br, "br_{}".format(image_name))), warped)
+            elif image_number == 3:
+                cv2.imwrite((os.path.join(outpath_tl, "tl_{}".format(image_name))), warped)
+            elif image_number == 4:
+                cv2.imwrite((os.path.join(outpath_tr, "tr_{}".format(image_name))), warped)
+                image_number = 1
+            image_number += 1
+
+def master(inPath, mainPath, species_image):
+    cnts = get_contours(species_image)
+    for image_name in os.listdir(inPath):  #reads all images in input folder, inPath
+        input_path = os.path.join(inPath, image_name)
+        image_split(input_path, mainPath, image_name, cnts)
+
+master(r"D:\VIP\All\6", r"D:\VIP\2", r"D:\VIP\All\6\top0076.tif")
